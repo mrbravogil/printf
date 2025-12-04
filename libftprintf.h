4615a16dfdbf2bb808cb3596aab3925ec31aadb4
Maria Rosa Bravo Gil <mabravo-@c3r4s5.42malaga.com>
2025-12-04 10:15:46 +0100

    primer commit

---
+++ libftprintf.h
@@ -0,0 +1,23 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   libftprintf.h                                      :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: mabravo- <mabravo-@student.42malaga.com>   +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/12/02 13:40:30 by mabravo-          #+#    #+#             */
+/*   Updated: 2025/12/02 13:40:34 by mabravo-         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef LIBFT_H
+# define LIBFT_H
+
+# include <unistd.h>
+# include <stdlib.h>
+# include <stddef.h>
+# include <stdio.h>
+# include <string.h>
+
+#endif
+
